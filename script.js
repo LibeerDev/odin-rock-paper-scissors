e76af1839f48be731d1add7debcc50083fcd9e5a
LibeerDev <libeergithub@gmail.com>
2023-08-08 19:21:54 +0200

    added sounds and buttons images

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
 let playerScore = 0;
 let computerScore = 0;
 
+
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 const character = document.querySelector(".character");
-const robot = document.querySelector(".robot")
+const robot = document.querySelector(".robot");
+const backgroundMusic = document.querySelector(".bgMusic");
+const buttonsDiv = document.querySelector(".buttonsDiv");
+const startButton = document.querySelector('.startGameButton')
+const resultsDiv = document.querySelector('.displayResults');
+const scoreDiv = document.querySelector('.displayScore');
+const header = document.querySelector('.header')
+const rockSound = document.querySelector('.rockSound');
+const paperSound = document.querySelector('.paperSound');
+const scissorsSound = document.querySelector('.scissorsSound');
 
 rock.addEventListener('click', () => playRound("rock"));
 paper.addEventListener('click', () => playRound("paper"));
 scissors.addEventListener('click', () => playRound("scissors"));
-
-const startButton = document.querySelector('.startGameButton')
-const resultsDiv = document.querySelector('.displayResults');
-const scoreDiv = document.querySelector('.displayScore');
-
 startButton.addEventListener('click', () => loadGame());
 
+function playMusic(){
+    backgroundMusic.play();
+}
+
 function loadGame(){
-    const allElements = document.querySelectorAll('body *');
-    allElements.forEach(element => {
-        element.style.display = 'none';
-      });
 
-      character.style.display = 'block';
-      character.classList.add('animated');
-      character.style.opacity = '100%';
+    startButton.style.display = 'none';
+    header.style.display = 'none';
 
-      robot.style.display = 'block';
-      robot.classList.add('animated');
-      robot.style.opacity = '100%';
-      
+    character.style.display = 'block';
+    character.classList.add('animated');
+    character.style.opacity = '100%';
+
+    robot.style.display = 'block';
+    robot.classList.add('animated');
+    robot.style.opacity = '100%';
+
+    rock.style.display = 'flex';
+    paper.style.display = 'flex';
+    scissors.style.display = 'flex';
+
 }
 
 function getComputerChoice()
@@ -46,26 +58,34 @@ function playRound(choice)
 
     // the game begins
     if (playerSelection === "rock" && computerSelection === "rock") {
+        rockSound.play();
         resultsDiv.textContent = "Rock meets rock. It's a tie.";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
+        rockSound.play();
         resultsDiv.textContent = "Rock meets paper. You lose!";
         computerScore += 1;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        rockSound.play();
         resultsDiv.textContent = "Rock meets scissors. You win!";
         playerScore += 1;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        paperSound.play();
         resultsDiv.textContent = "Paper meets scissors. You lose!";
         computerScore += 1;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        paperSound.play();
         resultsDiv.textContent = "Paper meets rock. You win!";
         playerScore += 1;
     } else if (playerSelection === "paper" && computerSelection === "paper") {
+        paperSound.play();
         resultsDiv.textContent = "Scissors meet paper. You win!";
         playerScore += 1;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        scissorsSound.play();
         resultsDiv.textContent = "Scissors meet rock. You lose!";
         computerScore += 1;
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        scissorsSound.play();
         resultsDiv.textContent = "Scissors meet scissors. It's a tie!";
     }
 
